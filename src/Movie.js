882590ff02014f2087bc5f43f3ea355b81ef8636
balabala <balabba112@gmail.com>
2023-01-23 21:17:25 +0530

    first changes

---
+++ src/Movie.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Movies(){
+    return (
+        <>
+        <div className="top">
+        <ol>
+            <li>Transformers</li>
+            <li>Sing 2</li>
+            <li>Trolls </li>
+            <li>Glass Onion: A Knives Out Mystery</li>
+            <li>Dog Gone</li>
+        </ol>
+        </div>
+        </>
+    )
+}
+export default Movies;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Mov from './Movie';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+<>
+<h1>Top 5 movie on Netflix</h1>
+<Mov></Mov>
+</>,document.getElementById('root'));
